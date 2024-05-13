968d7f75b117d42f290747bfa4372566e6aa133c
NaimulHasan <mohammadfahimshah@gmail.com>
2024-05-13 22:17:45 +0600

    conflict resolved resolved

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use('/admin/widget', widgetRoutes);
 
 
 const PORT = process.env.PORT || 5000;
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log('Server running on port: '+PORT)))
 .catch((error) =>console.log(error.message));
